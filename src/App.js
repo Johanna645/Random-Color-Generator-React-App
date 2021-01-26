265f2d918a1b5436f15adb538677e123fd332267
Johanna Räihälä <johanna.raihala@gmail.com>
2021-01-26 15:29:54 +0100

    Add css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="App">Random Color Generator</h1>
       </header>
+
+      <body className="App-body">
+        <br />
+        <box className="App-box"></box>
+        <button className="App-button-hex">Hex-code</button>
+      </body>
+
+      <footer className="App-footer">
+        <button className="App-button">Randomize!</button>
+      </footer>
     </div>
   );
 }
