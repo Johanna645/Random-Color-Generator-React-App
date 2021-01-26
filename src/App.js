06486aef42ccd888fa61ecc41df787edf0cb7240
Johanna Räihälä <johanna.raihala@gmail.com>
2021-01-26 19:19:15 +0100

    Add onClick and randomizer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-//import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import randomize from './randomize.js';
+/*import randomColor from 'randomcolor';  to call it use randomcolor() */
 
 function App() {
+  const [boxColor, setBoxColor] = useState(
+    'goldenrod',
+  ); /*creating a state variable called boxColor*/
   return (
     <div>
       <header className="App-header">
         <h1 className="App">Random Color Generator</h1>
       </header>
 
-      <body className="App-body">
+      <div className="App-body">
+        {/* prints out color: and the boxColor */}
+        color: {boxColor};
         <br />
-        <box className="App-box"></box>
-        <button className="App-button-hex">Hex-code</button>
-      </body>
+        <div className="App-box" style={{ backgroundColor: boxColor }}></div>
+        <button className="App-button-hex"></button>
+      </div>
 
       <footer className="App-footer">
-        <button className="App-button">Randomize!</button>
+        <button
+          className="App-button"
+          onClick={() => {
+            const color = randomize(); /*setting value for color through randomize*/
+            console.log(color);
+            setBoxColor(color); /*updating the state variable*/
+          }}
+        >
+          Randomize!
+        </button>
       </footer>
     </div>
   );
